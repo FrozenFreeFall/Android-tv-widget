3814c24428dc593687746ac88f89a75879858ea0
frozen free fall <hailong.qiu@xgimi.com>
2015-01-20 14:29:21 +0800

    添加代码注释说明

--- source/src/com/androidtv/activity/MainActivity.java
+++ source/src/com/androidtv/activity/MainActivity.java
@@ -15,6 +15,9 @@ import com.androidtv.view.FocusRelativeLayout.FocusRelativeLayoutCallBack;
 import com.androidtv.view.ReflectionRelativeLayout;
 
 /**
+ * 首先需要将FocusRelativeLayout作为主布局. <br>
+ * 内部将 ReflectionRelativeLayout 作为边框，倒影显示的布局。<br>
+ * ReflectionRelativeLayout 属性：waterreflection（是否显示倒影，布尔值） <br>
  * 
  * @author Frozen Free Fall
  *
@@ -23,7 +26,7 @@ public class MainActivity extends Activity {
 	private static final String TAG_base = "MainActivity";
 	FocusBorderView mBorderView;
 	Handler mHandler = new Handler();
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,9 +34,9 @@ public class MainActivity extends Activity {
 		FocusRelativeLayout focusView = (FocusRelativeLayout) findViewById(R.id.focus1);
 		focusView.setBorderViewBg(R.drawable.focus_bound);
 		// focusView.setBorderViewBg(R.drawable.ic_white_border_none);
-		focusView.setBorderScale(1.2f, 1.2f);
-		focusView.setBorderViewSize(5, 5);
-		focusView.setReflectPadding(5);
+		focusView.setBorderScale(1.2f, 1.2f); // 放大比例.
+		focusView.setBorderViewSize(1, 1); // 如果移动边框带有阴影，将阴影的距离填写上去.
+		focusView.setReflectPadding(5); // 设置倒影和子控件之间的距离.
 		// focusView.setBorderShow(false);
 		focusView.setBorderTV(false); // 设置 tv还有手机像素.
 		focusView.setBorderShow(true); // 显示外边框.
@@ -43,6 +46,7 @@ public class MainActivity extends Activity {
 					@Override
 					public void onFocusOutChild(
 							ReflectionRelativeLayout reflectionRelativeLayout) {
+						// 可以在子控件失去焦点的时候设置一些子控件内部子控件的动画或者改变属性等等.
 						for (int i = 0; i < reflectionRelativeLayout
 								.getChildCount(); i++) {
 							View v = reflectionRelativeLayout.getChildAt(i);
