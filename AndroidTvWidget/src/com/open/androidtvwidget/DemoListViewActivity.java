028e1aaa081f7aac35cc642fb7265d9443f9c887
hailongqiu <356752238@qq.com>
2016-02-24 08:45:00 +0800

    update listview demo

--- AndroidTvWidget/src/com/open/androidtvwidget/DemoListViewActivity.java
+++ AndroidTvWidget/src/com/open/androidtvwidget/DemoListViewActivity.java
@@ -4,17 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.open.androidtvwidget.view.MainUpView;
-import com.open.androidtvwidget.view.SmoothListView;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.widget.ViewDragHelper;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver.OnGlobalFocusChangeListener;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -32,32 +30,43 @@ public class DemoListViewActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.demo_list_view);
 		this.mInflater = LayoutInflater.from(getApplicationContext());
-		SmoothListView listView = (SmoothListView) findViewById(R.id.listview);
+		ListView listView = (ListView) findViewById(R.id.listview);
 		mainUpView1 = (MainUpView) findViewById(R.id.mainUpView1);
 		mainUpView1.setUpRectResource(R.drawable.white_light_10);
 		mainUpView1.setShadowDrawable(null);
 		mainUpView1.setDrawUpRectPadding(10);
+		mainUpView1.setTranDurAnimTime(200);
 		initData();
 
 		listView.setAdapter(new DemoAdapter());
 		//
+		listView.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				if (view != null)
+					mainUpView1.setFocusView(view, 1.0f);
+			}
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+			}
+		});
 		listView.getViewTreeObserver().addOnGlobalFocusChangeListener(new OnGlobalFocusChangeListener() {
 			@Override
 			public void onGlobalFocusChanged(View oldFocus, View newFocus) {
-				if (newFocus != null) {
-					mainUpView1.setFocusView(newFocus, 1.2f);
-				}
-				if (oldFocus != null) {
-					mainUpView1.setUnFocusView(oldFocus);
-				}
+//				if (newFocus != null) {
+//					mainUpView1.setFocusView(newFocus, 1.2f);
+//				}
+//				if (oldFocus != null) {
+//					mainUpView1.setUnFocusView(oldFocus);
+//				}
 			}
 		});
-		listView.setSelection(0);
+//		listView.setSelection(0);
 	}
 
 	public void initData() {
 		data = new ArrayList<String>();
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 105; i++) {
 			String text = "item" + i;
 			data.add(text);
 		}
