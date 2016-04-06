48585f0ea0d7b3516984acf38855f60ccaac321d
Jack <pengjunkun@gmail.com>
2016-04-06 10:42:00 +0800

    fix the bug of choosing first in DemoGridViewActivity.java

--- AndroidTvWidget/src/com/open/androidtvwidget/DemoGridViewActivity.java
+++ AndroidTvWidget/src/com/open/androidtvwidget/DemoGridViewActivity.java
@@ -5,14 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.open.androidtvwidget.adapter.EffectNoDrawBridge;
-import com.open.androidtvwidget.utils.Utils;
-import com.open.androidtvwidget.view.MainUpView;
-
 import android.app.Activity;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnLayoutChangeListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -20,70 +16,105 @@ import android.widget.GridView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import com.open.androidtvwidget.adapter.EffectNoDrawBridge;
+import com.open.androidtvwidget.utils.Utils;
+import com.open.androidtvwidget.view.MainUpView;
+
 /**
  * GridView Demo测试.
  */
-public class DemoGridViewActivity extends Activity {
+public class DemoGridViewActivity extends Activity
+{
 
 	private List<Map<String, Object>> data;
 	private MainUpView mainUpView1;
 	private View mOldView;
-	
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.demo_grid_view);
 
-		GridView gridView = (GridView) findViewById(R.id.gridView);
+		final GridView gridView = (GridView) findViewById(R.id.gridView);
 		mainUpView1 = (MainUpView) findViewById(R.id.mainUpView1);
-		
+
 		/**
-		 * android 4.2有问题.
-		 * 需要使用EffectNoDrawBridge.
+		 * android 4.2有问题. 需要使用EffectNoDrawBridge.
 		 */
-		if (Utils.getSDKVersion() == 17) {
+		if (Utils.getSDKVersion() == 17)
+		{
 			mainUpView1.setEffectBridge(new EffectNoDrawBridge()); // 4.3以下版本边框移动.
-			EffectNoDrawBridge bridget = (EffectNoDrawBridge) mainUpView1.getEffectBridge();
+			EffectNoDrawBridge bridget = (EffectNoDrawBridge) mainUpView1
+					.getEffectBridge();
 			bridget.setTranDurAnimTime(200);
-		} 
-		
+		}
+
 		mainUpView1.setUpRectResource(R.drawable.white_light_10);
 		mainUpView1.setDrawUpRectPadding(12);
-		
+
 		getData();
 
 		String[] from = { "text" };
 		int[] to = { R.id.textView };
 
-		SimpleAdapter simpleAdapter = new SimpleAdapter(this, data, R.layout.item_gridview, from, to);
+		SimpleAdapter simpleAdapter = new SimpleAdapter(this, data,
+				R.layout.item_gridview, from, to);
 		gridView.setAdapter(simpleAdapter);
 		simpleAdapter.notifyDataSetChanged();
 		//
-		gridView.setOnItemSelectedListener(new OnItemSelectedListener() {
+		gridView.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
 			@Override
-			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id)
+			{
 				view.bringToFront();
 				mainUpView1.setFocusView(view, mOldView, 1.2f);
 				mOldView = view;
 			}
 
 			@Override
-			public void onNothingSelected(AdapterView<?> parent) {
+			public void onNothingSelected(AdapterView<?> parent)
+			{
 			}
 		});
-		gridView.setOnItemClickListener(new OnItemClickListener() {
+		gridView.setOnItemClickListener(new OnItemClickListener()
+		{
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Toast.makeText(getApplicationContext(), "position : " + position, Toast.LENGTH_LONG).show();
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id)
+			{
+				Toast.makeText(getApplicationContext(),
+						"position : " + position, Toast.LENGTH_LONG).show();
 			}
 		});
 		// 不知道为何设置 0 不显示.(暂时无解)
-		gridView.setSelection(1);
+		// 在布局加咱完成后，设置选中第一个
+		gridView.addOnLayoutChangeListener(new OnLayoutChangeListener()
+		{
+
+			@Override
+			public void onLayoutChange(View v, int left, int top, int right,
+					int bottom, int oldLeft, int oldTop, int oldRight,
+					int oldBottom)
+			{
+				if (gridView.getChildCount() > 0)
+				{
+					gridView.setSelection(0);
+					mainUpView1.setFocusView(gridView.getChildAt(0), 1.2f);
+					mOldView = gridView.getChildAt(0);
+
+				}
+			}
+		});
 	}
 
-	public List<Map<String, Object>> getData() {
+	public List<Map<String, Object>> getData()
+	{
 		data = new ArrayList<Map<String, Object>>();
-		for (int i = 0; i < 200; i++) {
+		for (int i = 0; i < 200; i++)
+		{
 			Map<String, Object> map = new HashMap<String, Object>();
 			map.put("text", "item" + i);
 			data.add(map);
